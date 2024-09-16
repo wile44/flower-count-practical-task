500fc45395c7efe28488ba0b31a2301d6f9d7ec5
wile44 <goodluckwileonline@gmail.com>
2024-09-16 17:50:42 +0300

    feat: add some more pictures

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,6 @@ import torch
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security.api_key import APIKeyHeader
 
-
 app = FastAPI()
 
 origins = [
@@ -27,7 +26,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 API_KEY = "your_api_key_here"  
 api_key_header = APIKeyHeader(name="X-API-Key")
 
@@ -35,28 +33,29 @@ def authenticate(api_key: str = Depends(api_key_header)):
     if api_key != API_KEY:
         raise HTTPException(status_code=403, detail="Unauthorized")
 
-
 model_name = "smutuvi/flower_count_model"
 pipe = pipeline("object-detection", model=model_name)
-
 
 def extract_flower_count(output: list) -> int:
     """Extracts the flower count from the model's output."""
     flower_count = sum(1 for obj in output if obj['label'] == 'LABEL_1')
     return flower_count
 
-
 @app.get("/")
 def read_root():
     return {"message": "Flower Counting API"}
 
-
 @app.post("/process-image/")
 async def upload(files: List[UploadFile] = File(...)):
-    flower_counts = []  
+    flower_counts = [] 
+    
+    max_image_size = 5 * 1024 * 1024
     
     for file in files:
         
+        if file.size > max_image_size:
+            raise HTTPException(status_code=400, detail="Image size exceeds the maximum limit.")
+
         if not file.content_type.startswith("image/"):
             raise HTTPException(status_code=400, detail="Invalid file type. Please upload an image.")
 
@@ -66,13 +65,10 @@ async def upload(files: List[UploadFile] = File(...)):
             image_data = BytesIO(image_bytes)
             image = Image.open(image_data)
 
-            
             with torch.no_grad():
                 output = pipe(image)  
 
-            
             count = extract_flower_count(output)
-            
             
             flower_counts.append({"filename": file.filename, "flower_count": count})
 
@@ -81,7 +77,6 @@ async def upload(files: List[UploadFile] = File(...)):
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Error processing image {file.filename}: {str(e)}")
 
-    
     return {"results": flower_counts}
 
 
@@ -95,20 +90,16 @@ async def process_dataset():
 
     flower_counts = []
 
-    
     for image_file in os.listdir(dataset_directory):
         if image_file.endswith(('.png', '.jpg', '.jpeg')):
             try:
                 image_path = os.path.join(dataset_directory, image_file)
                 image = Image.open(image_path)
 
-                
                 with torch.no_grad():
                     output = pipe(image)
-
                 
                 count = extract_flower_count(output)
-
                 
                 flower_counts.append({"filename": image_file, "flower_count": count})
 
